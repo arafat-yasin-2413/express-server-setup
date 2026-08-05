728db4e0ac0b9910c9bd4af5d8cefffb56fde2a5
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-08-05 08:35:25 +0600

    fixes in the catch-async that was not sending the actual error to the global-error-handler

--- src/utils/catchAsync.ts
+++ src/utils/catchAsync.ts
@@ -2,10 +2,8 @@ import type { NextFunction, Request, RequestHandler, Response } from "express";
 import {error} from 'node:console'
 
 const catchAsync = (fn: RequestHandler) => (req: Request, res:Response, next:NextFunction) => {
-    Promise.resolve(fn(req,res,next)).catch(error)
-    {
-        next(error)
-    }
+    Promise.resolve(fn(req,res,next)).catch(next)
+    
 }
 
 export default catchAsync;
