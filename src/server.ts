9e509d39180e8c2c47c50608d6b6307e5743bd8e
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-08-05 09:52:19 +0600

    adds http server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,24 @@
 import dotenv from "dotenv";
 import { app } from "./app.js";
 import { env } from "./config/env.js";
+import http, { type Server } from "http";
 dotenv.config();
 
 const port = env.port;
 
-app.listen(port, () => {
-    console.log(`Server is running on port : ${port}`);
-});
+// in production, we use httpServer
+let server: Server
+
+const httpServer = http.createServer()
+server = httpServer.listen(port, ()=>{
+    console.log(`Http Server is Running on port : ${port}`)
+})
+
+
+
+// express server of node js.
+// jodio , behind the scene eta httpServer kei call dicche.
+// app.listen(port, () => {
+//     console.log(`Server is running on port : ${port}`);
+// });
+
