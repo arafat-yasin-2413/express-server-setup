6298ac125253aec9e430e52fb50c0311bea2b087
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-08-05 08:45:49 +0600

    shows the error stack

--- src/utils/ApiResponse.ts
+++ src/utils/ApiResponse.ts
@@ -1,22 +1,35 @@
 import type { Response } from "express";
 
-const success = (res: Response, statusCode: number, message: string, data: any) =>{
+const success = (
+    res: Response,
+    statusCode: number,
+    message: string,
+    data: any,
+) => {
     return res.status(statusCode).json({
         success: true,
         statusCode,
         message,
         data,
-    })
-}
+    });
+};
 
-const error = (res: Response, statusCode: number, message: string) =>{
+const error = (
+    res: Response,
+    statusCode: number,
+    message: string,
+    error: any,
+) => {
     return res.status(statusCode).json({
         success: false,
         statusCode,
         message,
-    })
-}
+        // stack: error.stack,
+        ...(process.env.NODE_ENV === "development" && { stack: error.stack }),
+    });
+};
 
 export const ApiResponse = {
-    success,error
-}
+    success,
+    error,
+};
