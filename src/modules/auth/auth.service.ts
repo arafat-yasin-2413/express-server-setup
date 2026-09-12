f5e0424ddb4336de19f37685dc469998a086d556
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-09-12 23:31:02 +0600

    minor changes

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -6,10 +6,10 @@ const userLogin = async (email: string, password: string) => {
         password: "123456",
     };
 
-    if (!user) throw new AppError(404,"user not found!");
+    if (!user) throw new AppError(404, "user not found!");
 
     if (user.email !== email || user.password !== password)
-        throw new AppError(403,"Invalid email or password");
+        throw new AppError(403, "Invalid email or password");
 
     return user
 };
